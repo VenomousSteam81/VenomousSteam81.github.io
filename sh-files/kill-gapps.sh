845e0cd6df20834149b2d8d238e0762db51624c7
Jordan <julianjordan353@gmail.com>
2025-02-23 18:21:27 -0600

    Update kill-gapps.sh

--- sh-files/kill-gapps.sh
+++ sh-files/kill-gapps.sh
@@ -5,9 +5,28 @@
 # All I really needed to kill was `com.android.vending` on my slow Lenovo 100e 2nd Gen MTK Chromebook, but I decided to add a bunch more.
 # Made by VSteam81
 
+apps=(
+    "com.google.android.ext.services"
+    "com.google.android.ext.shared"
+    "com.google.android.deskclock" 
+    "com.google.android.apps.cloudprint"
+    "com.google.android.contacts"
+    "com.google.android.syncadapters.contacts"
+    "com.google.android.gms"
+    "com.google.android.gsf"
+    "com.google.android.tts"
+    "com.google.android.feedback"
+    "com.google.android.printservice.recommendation"
+    "com.android.providers.telephony"
+    "com.android.providers.calendar"
+    "com.android.vending"
+    "com.android.printspooler"
+    "com.android.providers.contacts"
+)
+
 while true; do
-    for app in "com.google.android.ext.services" "com.google.android.ext.shared" "com.google.android.deskclock" "com.google.android.apps.cloudprint" "com.google.android.contacts" "com.google.android.syncadapters.contacts" "com.google.android.gms" "com.google.android.gsf" "com.google.android.tts" "com.google.android.feedback" "com.google.android.printservice.recommendation" "com.android.providers.telephony" "com.android.providers.calendar" "com.android.vending" "com.android.printspooler" "com.android.providers.contacts"; do
-        /system/bin/am force-stop com.android.vending
+    for app in "${apps[@]}"; do
+        /system/bin/am force-stop $app
         echo "killed $app"
     done
     sleep 0.5
