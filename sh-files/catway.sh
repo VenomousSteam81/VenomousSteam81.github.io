9a36270647a728999969cb4407a35bc14fe1f403
Jordan <julianjordan353@gmail.com>
2025-02-24 06:58:30 -0600

    Update catway.sh

--- sh-files/catway.sh
+++ sh-files/catway.sh
@@ -13,7 +13,7 @@ minecraft () {
   wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.jar
   wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u432-b06/OpenJDK8U-jre_x64_linux_hotspot_8u432b06.tar.gz
   tar xzf OpenJDK8U-jre_x64_linux_hotspot_8u432b06.tar.gz
-  echo "export PATH=/app/mc/jdk8u432-b06-jre:$PATH" > ~/.bashrc
+  echo "export PATH=/app/mc/jdk8u432-b06-jre:$PATH" >> ~/.bashrc
 }
 
 # Download/install packages
